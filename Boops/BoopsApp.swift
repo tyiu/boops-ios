2947babfccc75557b8ec1a0e6d3573cdd7ebcda5
Terry Yiu <963907+tyiu@users.noreply.github.com>
2024-03-19 21:29:06 -0400

    Initial Commit

---
+++ Boops/BoopsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoopsApp.swift
+//  Boops
+//
+//  Created by Terry Yiu on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct BoopsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
